6f82308b90a61010daf4a23bffbbc3385a001af4
Adam Nowak <lubieniebieski@gmail.com>
2025-07-08 23:19:27 +0200

    basic neovim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { style = "moon" },
+  },
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      colorscheme = "dracula",
+    },
+    config = function() 
+      vim.cmd.colorscheme("dracula")
+    end
+  },
+}
