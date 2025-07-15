4313cbca0e3470caca30a14474cc672ded503f3f
Adam Nowak <lubieniebieski@gmail.com>
2025-07-15 15:55:25 +0200

    stick with tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,15 @@
 return {
-  -- tokyonight
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "moon" },
   },
-  -- add dracula
-  { "Mofiqul/dracula.nvim" },
-
-  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     lazy = false,
     priority = 1000,
-    opts = {
-      colorscheme = "dracula",
-    },
     config = function()
-      vim.cmd.colorscheme("dracula")
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
